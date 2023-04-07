00d49d2b5a7d9e69676cc2612bd57252aaf69fcb
Rémi Michel <remi.michel38@gmail.com>
2023-04-07 10:04:50 +0200

    feat(): Session du 07/04

--- FrontEnd/assets/js/admin.js
+++ FrontEnd/assets/js/admin.js
@@ -19,26 +19,35 @@ if (token != null) {
     const buttonEditWorks = document.querySelector('#works-modify')
     const modalBg = document.querySelector('.modal-bg')
     const modal = document.querySelector('.modal')
+    const modalContent = document.querySelector('.modal-container ul')
     const cross = document.querySelector('.cross')
     const addImg = document.querySelector('.add-img')
     const modalAdd = document.querySelector('.modal-add')
+    const modalArrow = document.querySelector('.modal .arrow')
 
     buttonEditWorks.addEventListener('click', () => {
         modal.style.display = 'block'
+        modalBg.style.display = 'block'
     })
     
     modalBg.addEventListener('click', () => {
         modal.style.display = 'none'
+        modalBg.style.display = 'none'
     })
 
-    cross.addEventListener('click', (modalBg) => {
+    cross.addEventListener('click', () => {
         modal.style.display = 'none'
+        modalBg.style.display = 'none'
+    })
+
+    modalArrow.addEventListener('click', (modalBg) => {
+        modalContent.style.transform = 'translateX(0)'
+        modalArrow.style.display = 'none'
     })
 
     addImg.addEventListener('click', () => {
-        modal.style.display = 'none',
-        modalAdd.style.display = 'block',
-        modalBg.style.display = 'block';
+        modalContent.style.transform = 'translateX(-100%)'
+        modalArrow.style.display = 'block'
     })
 
     cross.addEventListener('click', (modalAdd) => {
