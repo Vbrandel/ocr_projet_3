93d8ec7a17c32b6e81f1f93a9f1fbb0094ad5eb2
Vbrandel <v.brandel76@gmail.com>
2023-04-14 01:39:10 +0200

    api

---
+++ FrontEnd/assets/js/api.js
@@ -0,0 +1,27 @@
+async function getWorks(){
+    const works = await fetch('http://localhost:5678/api/works')
+    return await works.json()
+}
+
+async function getCategories(){
+    const categories = await fetch('http://localhost:5678/api/categories')
+    return await categories.json()
+}
+
+async function deleteWork(workId){
+    return await fetch(`http://localhost:5678/api/works/${workId}`, {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('token')}`
+        }
+    })
+}
+
+async function addWork(){
+    return await fetch(`http://localhost:5678/api/works`, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('token')}`
+        }
+    })
+}
