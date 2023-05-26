43c530bad6c332420da87e56cf202fc6c29d3a00
Brandel <v.brandel76@gmail.com>
2023-05-26 15:41:40 +0200

    fix delete all

--- FrontEnd/assets/js/admin.js
+++ FrontEnd/assets/js/admin.js
@@ -159,7 +159,6 @@ function adminInit() {
                     dataListId.remove()
                     dataId.remove()
                 }
-                window.location.reload();
             }
         }
 
